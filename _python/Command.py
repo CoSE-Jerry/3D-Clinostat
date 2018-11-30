a9d3fe9b7addd53114a158fb622fd02314a182bb
Jerry Miao <jerry@coseinstruments.com>
2018-11-30 05:53:47 -0600

    Update Command.py

--- _python/Command.py
+++ _python/Command.py
@@ -10,21 +10,21 @@ def top_color_change(self):
         Settings.ASD.write(bytes('0', 'UTF-8'))
 
 def left_color_change(self):
-    temp = self.Left_Select.currentIndex()
+    temp = self.leftColor_comboBox.currentIndex()
     if temp == 1:
         Settings.ASD.write(bytes('2', 'UTF-8'))
     else:
         Settings.ASD.write(bytes('0', 'UTF-8'))
 
 def right_color_change(self):
-    temp = self.Right_Select.currentIndex()
+    temp = self.rightColor_comboBox.currentIndex()
     if temp == 1:
         Settings.ASD.write(bytes('3', 'UTF-8'))
     else:
         Settings.ASD.write(bytes('0', 'UTF-8'))
 
 def bottom_color_change(self):
-    temp = self.Top_Color_Select.currentIndex()
+    temp = self.bottomColor_comboBox.currentIndex()
     if temp == 1:
         Settings.ASD.write(bytes('4', 'UTF-8'))
     else:
