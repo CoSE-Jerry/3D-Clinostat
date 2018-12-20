f83ccb7a9ab7b291dbe664b5a91720a471b41cf7
Jerry Miao <jerry@coseinstruments.com>
2018-12-20 09:40:57 -0600

    update

--- _arduino/demo/demo.ino
+++ _arduino/demo/demo.ino
@@ -6,6 +6,32 @@
 #define QUARTER NUM_LEDS/4
 #define COMMANDSIZE 7
 
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+
+const int buzzerPin = 3;
+const int ledPin1 = 12;
+const int ledPin2 = 13;
+
+int counter = 0;
+
 
 int serial_CMD;
 boolean IR = false;
@@ -23,6 +49,11 @@ SoftwareSerial innerSerial(4, 5); // RX, TX
 SoftwareSerial outterSerial(6, 7); // RX, TX
 
 void setup() {
+
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+
   Serial.begin(9600);
   Serial.setTimeout(5);
 
@@ -59,10 +90,10 @@ void loop() {
       brightnessUpdate();
 
     else if (commands[0] == 3)
-      digitalWrite(10, LOW);
+      firstSection();
 
     else if (commands[0] == 4)
-      digitalWrite(10, HIGH);
+      secondSection();
 
     else if (commands[0] == 5)
       digitalWrite(9, LOW);
@@ -173,4 +204,83 @@ void clearCommands() {
   {
     commands[i] = 0;
   }
+}
+
+void beep(int note, int duration)
+{
+  //Play tone on buzzerPin
+  tone(buzzerPin, note, duration);
+
+  //Play different LED depending on value of 'counter'
+  if (counter % 2 == 0)
+  {
+    digitalWrite(ledPin1, HIGH);
+    delay(duration);
+    digitalWrite(ledPin1, LOW);
+  } else
+  {
+    digitalWrite(ledPin2, HIGH);
+    delay(duration);
+    digitalWrite(ledPin2, LOW);
+  }
+
+  //Stop tone on buzzerPin
+  noTone(buzzerPin);
+
+  delay(50);
+
+  //Increment counter
+  counter++;
+}
+
+void firstSection()
+{
+  beep(a, 500);
+  beep(a, 500);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+
+  delay(500);
+}
+
+void secondSection()
+{
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);
+  beep(fSH, 250);
+
+  delay(325);
+
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);
+  beep(cSH, 175);
+  beep(cH, 125);
+  beep(b, 125);
+  beep(cH, 250);
+
+  delay(350);
 }
