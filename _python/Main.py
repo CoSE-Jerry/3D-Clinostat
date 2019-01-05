4caf108e52145298910d90b5f58dbd720189cf32
Jerry Miao <jerry@coseinstruments.com>
2019-01-04 18:31:06 -0600

    Update Main.py

--- _python/Main.py
+++ _python/Main.py
@@ -131,7 +131,7 @@ class MainWindow(QMainWindow, Clinostat_UI.Ui_MainWindow):
 
     def ergz(self):
         Settings.ASD.write(bytes("7~0~", 'UTF-8'))
-        time.sleep(1)
+        time.sleep(0.05)
         Settings.ASD.write(bytes("8~0~", 'UTF-8'))
         
         
