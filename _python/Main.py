1137b0b8478236f1ea44d77240c0eb85d2080ab5
Jerry Miao <jerry@coseinstruments.com>
2019-01-09 12:55:37 -0600

    Update Main.py

--- _python/Main.py
+++ _python/Main.py
@@ -163,7 +163,7 @@ class MainWindow(QMainWindow, Clinostat_UI.Ui_MainWindow):
         try:
             self.Snap_Thread = Threads.Snap()
             self.Snap_Thread.start()
-            sleep(10)
+            sleep(15)
             os.system("/home/pi/Dropbox-Uploader/dropbox_uploader.sh download /3D_Clinostat/Snapshot/Snapshot.jpg")
             self.Image_Frame.setPixmap(QtGui.QPixmap("/home/pi/Snapshot.jpg"))
             
