b4e617ffc1bb13513de70e045acc40ce95fba948
Jerry Miao <jerry@coseinstruments.com>
2019-01-09 12:25:43 -0600

    Update Core.py

--- _python/Core.py
+++ _python/Core.py
@@ -33,7 +33,7 @@ def dataTransfer(conn):
             SnapShot = SnapShotProgram()
             SnapShotThread = Thread(target=SnapShot.run) 
             SnapShotThread.start()
-        print("waiting")
+            print("starting")
 
 class SnapShotProgram:  
     def __init__(self):
