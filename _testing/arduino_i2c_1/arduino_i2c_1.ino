991c063a4a798077f76cc9393ab36efb16225802
Jerry@Starting_block <jerry@coseinstruments.com>
2019-04-16 09:11:13 -0500

    update

--- _testing/arduino_i2c_1/arduino_i2c_1.ino
+++ _testing/arduino_i2c_1/arduino_i2c_1.ino
@@ -1,47 +1,75 @@
-/*
-  I2C Pinouts
-
-  SDA -> A4
-  SCL -> A5
-*/
-
-//Import the library required
 #include <Wire.h>
 
 //Slave Address for the Communication
 #define SLAVE_ADDRESS 0x08
+#define COMMANDSIZE 2
 
 char number[50];
+String CMD = "";
+char sz[] = "0~000~000~000~000~000";
+int commands[COMMANDSIZE];
 
 //Code Initialization
 void setup() {
-  // initialize i2c as slave
   Serial.begin(9600);
   Wire.begin(SLAVE_ADDRESS);
-  // define callbacks for i2c communication
   Wire.onReceive(receiveData);
-  //  Wire.onRequest(sendData);
 }
 
 void loop() {
   delay(1000);
-  Serial.print(number);
-} // end loop
+  if (CMD != NULL)
+  {
+    readCMD();
+    printCMD();
+    CMD = "";
+     Serial.println("called");
+  }
 
-// callback for received data
+}
+
+void readCMD()
+{
+  int current = 0;
+  clearCMD();
+
+  char buf[sizeof(sz)];
+  CMD.toCharArray(buf, sizeof(buf));
+  char *p = buf;
+  char *str;
+  while ((str = strtok_r(p, "~", &p)) != NULL)
+  {
+    int temp;
+    temp = atoi(str);
+    commands[current] = temp;
+    current++;
+  }
+  
+}
+
+void clearCMD()
+{
+  for (int i = 0; i < COMMANDSIZE; i++)
+  {
+    commands[i] = 0;
+  }
+}
+
+void printCMD()
+{
+  for (int i = 0; i < COMMANDSIZE; i++)
+  {
+    Serial.println(commands[i]);
+  }
+}
+
 void receiveData(int byteCount) {
   int i = 0;
   while (Wire.available()) {
     number[i] = Wire.read();
+    CMD += number[i];
     i++;
   }
   number[i] = '\0';
-
-}  // end while
-
-// callback for sending data
-void sendData() {
-  Wire.write(number);
 }
 
-//End of the program
--- _testing/rpi_i2c.py
+++ _testing/rpi_i2c.py
@@ -14,31 +14,28 @@ bus = smbus.SMBus(1)
 # This is the address we setup in the Arduino Program
 #Slave Address 1
 address = 0x08
+i2c_cmd = 0x01
 
 #Slave Address 2
-address_2 = 0x05
+#address_2 = 0x05
 
-def writeNumber(value):
-    bus.write_byte(address, value)
+def writeBlock(bytesToSend):
+    bus.write_i2c_block_data(address, i2c_cmd, bytesToSend)
+    #bus.write_byte(address, value)
     #bus.write_byte(address_2, value)
     # bus.write_byte_data(address, 0, value)
     return -1
 
-def readNumber():
-    # number = bus.read_byte(address)
-    number = bus.read_byte_data(address, 1)
-    return number
+def ConvertStringToBytes(src):
+    converted = []
+    for b in src:
+        converted.append(ord(b))
+    return converted
     
 while True:
 	#Receives the data from the User
     data = input("Enter the data to be sent : ")
-    data_list = list(data)
-    for i in data_list:
-    	#Sends to the Slaves 
-        writeNumber(int(ord(i)))
-        time.sleep(0.01)
-
-    writeNumber(int(0x0A))
-
+    tosend = ConvertStringToBytes(data)
+    writeBlock(tosend)
 #End of the Script
 
