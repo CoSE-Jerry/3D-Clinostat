8da9af204e51816a18a5b406e2f42a0d339da661
Jerry@Starting_block <jerry@coseinstruments.com>
2019-04-18 06:36:45 -0500

    Update Main.py

--- _python/Main.py
+++ _python/Main.py
@@ -16,15 +16,15 @@ import Clinostat_UI
 class MainWindow(QMainWindow, Clinostat_UI.Ui_MainWindow):
  # access variables inside of the UI's file
 
-     def link(self):
-    if(Settings.LINKED):
-        Settings.LINKED = False
-        self.frameLink_pushButton.setIcon(broken)
-        self.coreLink_pushButton.setIcon(broken)
-    else:
-        Settings.LINKED = True
-        self.frameLink_pushButton.setIcon(linked)
-        self.coreLink_pushButton.setIcon(linked)
+    def link(self):
+        if(Settings.LINKED):
+            Settings.LINKED = False
+            self.frameLink_pushButton.setIcon(broken)
+            self.coreLink_pushButton.setIcon(broken)
+        else:
+            Settings.LINKED = True
+            self.frameLink_pushButton.setIcon(linked)
+            self.coreLink_pushButton.setIcon(linked)
 
     def frame_slider_select(self):
         #if(Settings.LINKED):
