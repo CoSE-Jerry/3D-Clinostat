5df95832e20bff19278f9c15bc08bb738b45ab3a
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-06 16:11:08 -0500

    Revert "Update Core.py"
    This reverts commit 2b85fd63ef01891d9776a0a23de9ffbddba71ef3.

--- _python/Core.py
+++ _python/Core.py
@@ -9,39 +9,37 @@ sock.bind(server_address)
 
 sock.listen(1)
 
+while True:
+    connection, client_address = sock.accept()
+
     while True:
-        try:
-        connection, client_address = sock.accept()
+        recieved = connection.recv(1024).decode("utf-8")
 
-        while True:
-            recieved = connection.recv(1024).decode("utf-8")
+        CMD = recieved.split('~', 5)
 
-            CMD = recieved.split('~', 5)
-
-            if(CMD[0]=='A'):
-                with PiCamera() as camera:
-                    camera.resolution = (int(CMD[1]),int(CMD[2]))
-                    camera._set_rotation(90*int(CMD[3]))
-                    sleep(2)
-                    if(int(CMD[4])==1):
-                        camera.capture("out.jpg")
-                    else:
-                        camera.capture("out.png")
-                
+        if(CMD[0]=='A'):
+            with PiCamera() as camera:
+                camera.resolution = (int(CMD[1]),int(CMD[2]))
+                camera._set_rotation(90*int(CMD[3]))
+                sleep(2)
                 if(int(CMD[4])==1):
-                    f = open ("out.jpg", "rb")
+                    camera.capture("out.jpg")
                 else:
-                    f = open ("out.png", "rb")
+                    camera.capture("out.png")
+            
+            if(int(CMD[4])==1):
+                f = open ("out.jpg", "rb")
+            else:
+                f = open ("out.png", "rb")
+            l = f.read(1024)
+            while (l):
+                connection.send(l)
                 l = f.read(1024)
-                while (l):
-                    connection.send(l)
-                    l = f.read(1024)
-                f.close()
-                break
+            f.close()
+            break
 
-        connection.close()
-        except :
-            pass
+    connection.close()
+    
 
 
 
