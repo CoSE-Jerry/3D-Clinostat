a29ffaf6731dbfabbbdfa60c0de6242f3be83ea2
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-06 10:22:42 -0500

    Update Commands.py

--- _python/Commands.py
+++ _python/Commands.py
@@ -3,6 +3,8 @@ from time import sleep
 import socket
 import pigpio
 
+pi = pigpio.pi()
+
 def light_confirm(self):
     Settings.sendCMD(Settings.lighting_addr,"1~"+str(self.Start_spinBox.value())+"~"+str(self.End_spinBox.value())+"~"+ str(self.R_spinBox.value()) + "~" + str(self.G_spinBox.value()) + "~" + str(self.B_spinBox.value()) + "~"+str(self.W_spinBox.value()))
     sleep(0.1)
@@ -116,11 +118,10 @@ def IR_trigger(self):
 def sensor_check():
 
     try:
-        pi2 = pigpio.pi()
-        h3 = pi.i2c_open(1,31)
-        pi.i2c_read_byte(h3)
-        pi.i2c_close(h3)
-        pi2.stop
+        
+        h = pi.i2c_open(1,31)
+        pi.i2c_read_byte(h)
+        pi.i2c_close(h)
         Settings.sensor_attached = True
     except:
         Settings.sensor_attached = False
