2414d971f951bfb185ab1567ca7440478c9e96ef
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-06 11:26:18 -0500

    Update Main.py

--- _python/Main.py
+++ _python/Main.py
@@ -97,7 +97,7 @@ class MainWindow(QMainWindow, Clinostat_UI.Ui_MainWindow):
             self.Timelapse_Thread.transmit.connect(lambda: UI_Update.transmit_update(self))
 
             self.Timelapse_Thread.started.connect(lambda: UI_Update.timelapse_start(self))
-            self.Timelapse_Thread.captured.connect(lambda: UI_Update.image_update(self))
+            self.Timelapse_Thread.captured.connect(lambda: UI_Update.image_captured(self))
             self.Timelapse_Thread.finished.connect(lambda: UI_Update.timelapse_end(self))
 
             self.Timelapse_Thread.start()
