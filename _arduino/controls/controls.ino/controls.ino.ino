adaf5888966970aaae3e78f2d191ce4d29400cd0
Jerry@Starting_block <jerry@coseinstruments.com>
2019-05-06 21:18:47 -0500

    update

---
+++ _arduino/controls/controls.ino/controls.ino.ino
@@ -0,0 +1,137 @@
+//Import the library required
+#include <Wire.h>
+#include <Adafruit_NeoPixel.h>
+
+//Slave Address for the Communication
+#define PIN 6
+#define NUM_LEDS 35
+#define BRIGHTNESS 50
+#define QUARTER NUM_LEDS/4
+#define SLAVE_ADDRESS 0x08
+#define COMMANDSIZE 7
+
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRBW + NEO_KHZ800);
+
+char data[50];
+int commands[COMMANDSIZE];
+boolean IR = false;
+
+//Code Initialization
+void setup() {
+  // initialize i2c as slave
+  Serial.begin(9600);
+  Wire.begin(SLAVE_ADDRESS);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  //  Wire.onRequest(sendData);
+
+  strip.setBrightness(BRIGHTNESS);
+  strip.begin();
+  strip.show();
+
+  colorWipe(strip.Color(100, 50, 50, 50), 3);
+  colorWipe(strip.Color(50, 100, 50, 50), 3);
+  colorWipe(strip.Color(50, 50, 100, 50), 3);
+  colorWipe(strip.Color(50, 50, 50, 100), 3);
+  colorWipe(strip.Color(0, 0, 0, 0), 1);
+  pinMode(7, OUTPUT);
+  digitalWrite(7, LOW);
+
+
+}
+
+void loop() {
+}
+
+// callback for received data
+void receiveData(int byteCount) {
+  int i = 0;
+  if (Wire.read() == '^')
+  {
+    while (Wire.available()) {
+      data[i] = Wire.read();
+      i++;
+    }
+    data[i] = '\0';
+    Serial.println(data);
+    processCMD();
+    exeCMD();
+  }
+}  // end while
+
+void processCMD() {
+  clearCMD();
+  int current = 0;
+  char *p = data;
+  char *str;
+  while ((str = strtok_r(p, "~", &p)) != NULL)
+  {
+    int temp;
+    temp = atoi(str);
+    commands[current] = temp;
+    current++;
+  }
+
+}
+
+void clearCMD() {
+  for (int i = 0; i < COMMANDSIZE; i++)
+  {
+    commands[i] = 0;
+  }
+}
+
+void printCMD() {
+  for (int i = 0; i < COMMANDSIZE; i++)
+  {
+    Serial.println(commands[i]);
+  }
+}
+
+void exeCMD() {
+  if (commands[0] == 1)
+  {
+    stripUpdate();
+  }
+
+  if (commands[0] == 2)
+  {
+    brightnessUpdate();
+  }
+
+  if (commands[0] == 3)
+  {
+    if (!IR)
+    {
+      digitalWrite(7, HIGH);
+    }
+    else
+    {
+      digitalWrite(7, LOW);
+    }
+    IR = !IR;
+  }
+}
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    strip.setPixelColor(i, c);
+    strip.show();
+    delay(wait);
+  }
+}
+
+void stripUpdate() {
+  for (uint16_t i = 0; i < strip.numPixels(); i++) {
+    if (i >= commands[1] && i < commands[2]) {
+      strip.setPixelColor(i, commands[3], commands[4], commands[5], commands[6]);
+    }
+    strip.show();
+  }
+}
+
+void brightnessUpdate() {
+  strip.setBrightness(commands[1]);
+  strip.show();
+}
+
